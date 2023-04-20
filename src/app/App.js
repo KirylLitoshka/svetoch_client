759031be6e53f448497890abedeb464b3b38be20
Kirill Litoshko <kovzik08@gmail.com>
2023-04-20 16:43:17 +0300

    added app router

--- src/app/App.js
+++ src/app/App.js
@@ -1,10 +1,19 @@
 import "./App.css";
+import React from "react";
 import PageWrapper from "../components/wrappers/page/PageWrapper";
+import Header from "../components/ui/header/Header";
+import MainRouter from "../routes/main/MainRouter";
+import { BrowserRouter as Router } from "react-router-dom";
 
 const App = () => {
   return (
     <PageWrapper>
-      
+      <Router>
+        <Header />
+        <MainRouter>
+
+        </MainRouter>
+      </Router>
     </PageWrapper>
   );
 };
---
+++ src/routes/main/MainRouter.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+
+const MainRouter = () => {
+  return (
+    <Routes>
+      <Route path="/" element={<div>asd</div>} />
+    </Routes>
+  );
+};
+
+export default MainRouter;
