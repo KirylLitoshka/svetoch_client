038cdbdda8b4134827bc491f1b8a66adc388efa1
Kirill Litoshko <kovzik08@gmail.com>
2023-04-26 11:55:49 +0300

    added page for ciphers items

---
+++ src/pages/ciphers/Ciphers.jsx
@@ -0,0 +1,92 @@
+import axios from "axios";
+import React, { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+import CatalogueWrapper from "../../components/wrappers/catalogue/CatalogueWrapper";
+import Loader from "../../components/ui/loader/Loader";
+import Error from "../../components/ui/error/Error";
+import CatalogueItem from "../../components/items/catalogue/CatalogueItem";
+import CatalogueControl from "../../components/ui/controls/catalogue/CatalogueControl";
+import ControlButton from "../../components/ui/buttons/controls/ControlButton";
+import ModalWrapper from "../../components/wrappers/modal/ModalWrapper";
+import Confirm from "../../components/ui/modals/confirm/Confirm";
+import PageTitle from "../../components/ui/title/PageTitle";
+
+const Ciphers = () => {
+  const [ciphers, setCiphers] = useState([]);
+  const [error, setError] = useState("");
+  const [isLoading, setLoading] = useState(true);
+  const [selectedID, setSelectedID] = useState(null);
+  const [modalVisible, setModalVisible] = useState(false);
+
+  const getCiphers = async () => {
+    axios
+      .get("/api/v1/electricity/ciphers")
+      .then((r) => setCiphers(r.data))
+      .catch((e) => setError(e.response.data.reason))
+      .then(() => setLoading(false));
+  };
+
+  const deleteCiphers = async () => {
+    axios
+      .delete(`/api/v1/electricity/ciphers/${selectedID}`)
+      .then((r) => {
+        if (r.data.status === "success") {
+          setCiphers(ciphers.filter((cipher) => cipher.id !== selectedID));
+        } else {
+          setError(r.data.reason);
+        }
+      })
+      .catch((e) => setError(e.response.data.reason))
+      .then(() => setModalVisible(false));
+  };
+
+  useEffect(() => {
+    getCiphers();
+  }, []);
+
+  if (isLoading) {
+    return <Loader />;
+  } else if (error) {
+    return <Error message={error} />;
+  }
+
+  return (
+    <React.Fragment>
+      <PageTitle>
+        <Link to="add" className="page-main__button">
+          + Добавить
+        </Link>
+      </PageTitle>
+      <CatalogueWrapper>
+        {ciphers.map((cipher) => (
+          <CatalogueItem key={cipher.id} title={cipher.title}>
+            <div>Код: {cipher.code}</div>
+            <CatalogueControl>
+              <ControlButton
+                type="link"
+                label="Изменить"
+                linkState={{ id: cipher.id }}
+              />
+              <ControlButton
+                label={"Удалить"}
+                type="button"
+                callback={() => {
+                  setSelectedID(cipher.id);
+                  setModalVisible(true);
+                }}
+              />
+            </CatalogueControl>
+          </CatalogueItem>
+        ))}
+      </CatalogueWrapper>
+      <ModalWrapper isVisible={modalVisible} setIsVisible={setModalVisible}>
+        <Confirm
+          onConfirmAction={deleteCiphers}
+          onCloseAction={setModalVisible}
+        />
+      </ModalWrapper>
+    </React.Fragment>
+  );
+};
+
+export default Ciphers;
--- src/routes/main/MainRouter.jsx
+++ src/routes/main/MainRouter.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
 import Home from "../../pages/home/Home";
+import Ciphers from "../../pages/ciphers/Ciphers"
 
 const MainRouter = () => {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
+      <Route path="/catalogues/ciphers" element={<Ciphers />} />
     </Routes>
   );
 };
