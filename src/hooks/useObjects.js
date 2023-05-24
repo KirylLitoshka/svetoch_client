96b4d5ae99884417282e999aa04323b367da8601
Kiryl Litoshka <kirill.litoshko@gmail.com>
2023-05-24 14:27:20 +0300

    added hook for objects filter

---
+++ src/hooks/useObjects.js
@@ -0,0 +1,52 @@
+import {useMemo} from "react";
+
+export const useFilterByStatus = (objects, searchQuery) => {
+  return useMemo(() => {
+    if (searchQuery.status) {
+      return [...objects].filter((obj) => obj.is_closed === Boolean(+searchQuery.status))
+    }
+    return objects
+  }, [objects, searchQuery])
+}
+
+
+export const useFilterByMeter = (objects, searchQuery) => {
+  const filteredObjects = useFilterByStatus(objects, searchQuery)
+  return useMemo(() => {
+    if (searchQuery.meterNumber) {
+      return [...filteredObjects].filter((obj) => {
+        if (obj.meter?.number) {
+          console.log(obj)
+          return obj.meter.number.includes(searchQuery.meterNumber)
+        }
+        return false
+      })
+    }
+    return filteredObjects
+  }, [filteredObjects, searchQuery])
+}
+
+export const useFilterByCountingPoint = (objects, searchQuery) => {
+  const filteredObjects = useFilterByMeter(objects, searchQuery)
+  return useMemo(() => {
+    if (searchQuery.countingPoint) {
+      return [...filteredObjects].filter((obj) => {
+        if (obj.counting_point) {
+          return obj.counting_point.toString().includes(searchQuery.countingPoint)
+        }
+        return false
+      })
+    }
+    return filteredObjects
+  }, [filteredObjects, searchQuery])
+}
+
+export const useObjects = (objects, searchQuery) => {
+  const filteredObjects = useFilterByCountingPoint(objects, searchQuery)
+  return useMemo(() => {
+    if (searchQuery.title) {
+      return [...filteredObjects].filter(obj => obj.title.toLowerCase().includes(searchQuery.title.toLowerCase()))
+    }
+    return filteredObjects
+  }, [filteredObjects, searchQuery])
+};
