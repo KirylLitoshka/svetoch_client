3d863cbdf6fa6f265bac14d4a4c5a2792561d25d
Kiryl Litoshka <kirill.litoshko@gmail.com>
2023-06-27 11:12:37 +0300

    updated limits view from catalogue to accordion

--- src/pages/limits/Limits.jsx
+++ src/pages/limits/Limits.jsx
@@ -1,15 +1,16 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 import { Link } from "react-router-dom";
-import CatalogueWrapper from "../../components/wrappers/catalogue/CatalogueWrapper";
 import PageTitle from "../../components/ui/title/PageTitle";
-import CatalogueItem from "../../components/items/catalogue/CatalogueItem";
+import { showContent } from "../../utils/accordion";
+import Accordion from "../../components/ui/accordion/Accordion";
 import CatalogueControl from "../../components/ui/controls/catalogue/CatalogueControl";
 import ControlButton from "../../components/ui/buttons/controls/ControlButton";
 import Confirm from "../../components/ui/modals/confirm/Confirm";
 import ModalWrapper from "../../components/wrappers/modal/ModalWrapper";
 import Loader from "../../components/ui/loader/Loader";
 import Error from "../../components/ui/error/Error";
+import ObjectsList from "../objects/ObjectsList";
 
 const Limits = () => {
   const [limits, setLimits] = useState([]);
@@ -17,6 +18,8 @@ const Limits = () => {
   const [error, setError] = useState("");
   const [modalVisible, setModalVisible] = useState(false);
   const [selectedItem, setSelectedItem] = useState({});
+  const [objectsListModalVisible, setObjectsListModalVisible] = useState(false);
+  const [selectedID, setSelectedID] = useState(null);
 
   const deleteLimitItem = async () => {
     axios
@@ -64,32 +67,59 @@ const Limits = () => {
           + Добавить
         </Link>
       </PageTitle>
-      <CatalogueWrapper>
+      <Accordion>
         {limits.map((limit) => (
-          <CatalogueItem key={limit.id} title={limit.title}>
-            <CatalogueControl>
-              <ControlButton
-                label={"Изменить"}
-                type={"link"}
-                linkURL={"edit"}
-                linkState={{ item: limit }}
-              />
-              <ControlButton
-                type={"button"}
-                label={"Удалить"}
-                callback={() => {
-                  setModalVisible(true);
-                  setSelectedItem(limit);
-                }}
-              />
-            </CatalogueControl>
-          </CatalogueItem>
+          <div className="accordion-list_item" key={limit.id}>
+            <div className="accordion-list_item__button" onClick={showContent}>
+              {limit.title}
+            </div>
+            <div className="accordion__content-wrapper">
+              <div style={{ padding: "10px 0" }} className="accordion__content">
+                <div>Киловатт: кол-во киловатт</div>
+                <div>Количество точек: {limit.objects_amount}</div>
+              </div>
+              <CatalogueControl>
+                <ControlButton
+                  type={"button"}
+                  label={"Точки"}
+                  callback={() => {
+                    setSelectedID(limit.id);
+                    setObjectsListModalVisible(true);
+                  }}
+                />
+                <ControlButton
+                  label={"Изменить"}
+                  type={"link"}
+                  linkURL={"edit"}
+                  linkState={{ item: limit }}
+                />
+                <ControlButton
+                  type={"button"}
+                  label={"Удалить"}
+                  callback={() => {
+                    setModalVisible(true);
+                    setSelectedItem(limit);
+                  }}
+                />
+              </CatalogueControl>
+            </div>
+          </div>
         ))}
-      </CatalogueWrapper>
+      </Accordion>
       <ModalWrapper isVisible={modalVisible} setIsVisible={setModalVisible}>
         <Confirm
           onCloseAction={setModalVisible}
           onConfirmAction={deleteLimitItem}
+        />
+      </ModalWrapper>
+      <ModalWrapper
+        isVisible={objectsListModalVisible}
+        setIsVisible={setObjectsListModalVisible}
+      >
+        <ObjectsList
+          field={"limit"}
+          id={selectedID}
+          closeAction={setObjectsListModalVisible}
         />
       </ModalWrapper>
     </React.Fragment>
